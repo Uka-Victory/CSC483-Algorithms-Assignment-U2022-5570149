7433def13133aa000b5cde9eea756f2008a868e5
Uka-Victory <ukavictory20@gmail.com>
2026-04-05 16:34:36 +0100

    Delete src/com directory

--- src/com/csc483/assignment1/search/src/com/csc483/assignment2/sorting/SortStats.java
+++
@@ -1,9 +0,0 @@
-package com.csc483.assignment2.sorting;
-
-/**
- * Stores statistics for sorting algorithms.
- */
-public class SortStats {
-    public long comparisons = 0;
-    public long swaps = 0;
-}
